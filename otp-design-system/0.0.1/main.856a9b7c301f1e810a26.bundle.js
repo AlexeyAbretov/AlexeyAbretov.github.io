ef32bbad2469b551ba4d38d063c0a374654cae10
Абретов Алексей Александрович <a.abretov@otpbank.ru>
2020-10-29 13:55:14 +0300

    feat(design system): Дизайн система - 0.0.1

---
+++ otp-design-system/0.0.1/main.856a9b7c301f1e810a26.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1265:function(n,o,i){"use strict";i.r(o);var s=i(66);s.addons.setConfig({panelPosition:"right"})},1487:function(n,o,i){i(472),i(615),i(1265),i(1266),i(1371),i(1376),i(1406),n.exports=i(1488)},534:function(n,o){}},[[1487,1,2]]]);
